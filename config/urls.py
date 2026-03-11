01e614997b7743b228e1d344ef1585bb0dacbbb1
khamroyev2004 <uktamyoriyev2004@gmail.com>
2026-03-11 17:00:40 +0500

    sdf

--- config/urls.py
+++ config/urls.py
@@ -1,22 +1,19 @@
-
-from django.contrib import admin
-from django.urls import path, include
-from drf_yasg.views import get_schema_view
-from drf_yasg.views import get_schema_view
+from django.urls import path, re_path, include
 from rest_framework import permissions
+from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Mening API",
+        title="Dekanat API",
         default_version='v1',
-        description="API tavsifi",
+        description="Dekanat tizimi API hujjati",
+        contact=openapi.Contact(email="admin@admin.com"),
     ),
     public=True,
-    permission_classes=[permissions.AllowAny],
+    permission_classes=(permissions.AllowAny,),
 )
-
 urlpatterns = [
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-ui'),
-    path('api/',include('main.urls'))
+    path('api/',include('main.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0)),
 ]
---
+++ main/views.py
@@ -0,0 +1,22 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import APIView
+import requests
+from .models import *
+from .serializers import *
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets
+from rest_framework import  status
+
+
+
+
+
+
+
+
+
+
+
+
+
